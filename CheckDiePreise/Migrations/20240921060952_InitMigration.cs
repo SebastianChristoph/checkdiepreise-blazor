ea7ecf026f539b7edd368a273dd3d80bdd883439
SebastianChristoph <sebastianchristoph2@gmail.com>
2024-09-21 08:10:43 +0200

    Add Mgrations

---
+++ CheckDiePreise/Migrations/20240921060952_InitMigration.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CheckDiePreise.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DailyReports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Identifier = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Baseprice = table.Column<decimal>(type: "TEXT", nullable: false),
+                    BasepriceUnit = table.Column<string>(type: "TEXT", nullable: false),
+                    Store = table.Column<string>(type: "TEXT", nullable: false),
+                    Category = table.Column<string>(type: "TEXT", nullable: false),
+                    Url = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DailyReports", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductChanges",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Identifier = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    PriceBefore = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Baseprice = table.Column<decimal>(type: "TEXT", nullable: false),
+                    BasepriceBefore = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Difference = table.Column<decimal>(type: "TEXT", nullable: false),
+                    DifferenceBaseprice = table.Column<decimal>(type: "TEXT", nullable: false),
+                    BasepriceUnit = table.Column<string>(type: "TEXT", nullable: false),
+                    Store = table.Column<string>(type: "TEXT", nullable: false),
+                    Category = table.Column<string>(type: "TEXT", nullable: false),
+                    Trend = table.Column<string>(type: "TEXT", nullable: false),
+                    Url = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductChanges", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StorePriceChanges",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StoreName = table.Column<string>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Baseprice = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Category = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StorePriceChanges", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DailyReports");
+
+            migrationBuilder.DropTable(
+                name: "ProductChanges");
+
+            migrationBuilder.DropTable(
+                name: "StorePriceChanges");
+        }
+    }
+}
